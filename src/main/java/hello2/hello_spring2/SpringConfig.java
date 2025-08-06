7521f228295f9542f3d031d30ca58748a7fdfd15
hyunjun.jin <hyunjun6210@naver.com>
2025-08-06 19:02:43 +0900

    순수 JDBC

--- src/main/java/hello2/hello_spring2/SpringConfig.java
+++ src/main/java/hello2/hello_spring2/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello2.hello_spring2;
+import hello2.hello_spring2.repository.JdbcMemberRepository;
 import hello2.hello_spring2.repository.MemberRepository;
 import hello2.hello_spring2.repository.MemomryMemberRepository;
 import hello2.hello_spring2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemomryMemberRepository();
+//        return new MemomryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
